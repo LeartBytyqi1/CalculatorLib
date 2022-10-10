99e77dd1fe25329b1a9f4c5c9904ad74bfedc4ed
LeartBytyqi <lb51563@ubt-uni.net>
2022-10-10 13:57:05 +0200

    add calc class

---
+++ src/commonMain/kotlin/Calculator.kt
@@ -0,0 +1,15 @@
+class Calculator {
+    fun add(num1 : Double, num2 : Double) : Double {
+        return num1 + num2
+    }
+    fun sub(num1 : Double, num2 : Double) : Double {
+        return num1 - num2
+    }
+    fun mul(num1 : Double, num2 : Double) : Double {
+        return num1 * num2
+    }
+    fun div(num1 : Double, num2 : Double) : Double {
+        return num1 / num2
+    }
+
+}
